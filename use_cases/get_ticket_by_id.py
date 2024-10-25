d6f44422c8daf6746a0df93efa66462a3b9e1ed8
Angelo Netho <angelonetho@gmail.com>
2024-10-24 23:32:54 -0300

    adding eager loading to ticket and ticket_type readers

--- use_cases/get_ticket_by_id.py
+++ use_cases/get_ticket_by_id.py
@@ -1,11 +1,16 @@
 from db.connection import get_session
 from db.models.tickets import Ticket
+from sqlalchemy.orm import joinedload
 
 session = get_session()
 
 def execute(ticket_id):
     try:
-        ticket = session.query(Ticket).where(Ticket.id == ticket_id).first()
+        ticket = session.query(Ticket).options(
+            joinedload(Ticket.ticket_type),
+            joinedload(Ticket.customer),
+            joinedload(Ticket.employee)
+        ).filter(Ticket.id == ticket_id).first()
 
         if not ticket:
             raise Exception('Ticket not found.')
--- use_cases/get_ticket_type_by_id.py
+++ use_cases/get_ticket_type_by_id.py
@@ -1,11 +1,14 @@
 from db.connection import get_session
+from sqlalchemy.orm import joinedload
 from db.models.ticket_types import TicketType
 
 session = get_session()
 
 def execute(ticket_type_id):
     try:
-        ticket_type = session.query(TicketType).where(TicketType.id == ticket_type_id).first()
+        ticket_type = session.query(TicketType).options(
+            joinedload(TicketType.event)
+        ).filter(TicketType.id == ticket_type_id).first()
 
         if not ticket_type:
             raise Exception('Ticket type not found.')
