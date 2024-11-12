a1364f52959710d7c5e7206086fe7da192832720
Angelo Netho <angelonetho@gmail.com>
2024-11-11 23:05:15 -0300

    Implementa gerenciamento de transação para criação de ingresso

--- use_cases/create_ticket.py
+++ use_cases/create_ticket.py
@@ -5,28 +5,29 @@ from db.models.customers import Customer
 
 session = get_session()
 
-
 def execute(ticket_type_id, customer_id):
     try:
+        with session.begin():
+            ticket_type = (
+                session.query(TicketType).where(TicketType.id == ticket_type_id).first()
+            )
+            if not ticket_type:
+                raise Exception("Ticket Type not found.")
 
-        ticket_type = (
-            session.query(TicketType).where(TicketType.id == ticket_type_id).first()
-        )
+            customer = session.query(Customer).where(Customer.id == customer_id).first()
+            if not customer:
+                raise Exception("Customer not found.")
 
-        if not ticket_type:
-            raise Exception("Ticket Type not found.")
+            new_ticket = Ticket(ticket_type_id=ticket_type_id, customer_id=customer_id)
+            session.add(new_ticket)
 
-        customer = session.query(Customer).where(Customer.id == customer_id).first()
-
-        if not customer:
-            raise Exception("Customer not found.")
-
-        new_ticket = Ticket(ticket_type_id=ticket_type_id, customer_id=customer_id)
-
-        session.add(new_ticket)
-
-        session.commit()
+            session.commit()
 
         return new_ticket
+
+    except Exception as exception:
+        session.rollback()
+        raise exception
+
     finally:
         session.close()
